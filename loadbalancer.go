807572ca2de72700a4f60185f953e79e1464096f
Hernan Maschwitz <hernanm992@gmail.com>
2017-06-08 03:21:49 -0300

    list of servers

--- loadbalancer.go
+++ loadbalancer.go
@@ -4,18 +4,8 @@ import "gopkg.in/gin-gonic/gin.v1"
 import "net/http/httputil"
 import "net/url"
 import "fmt"
-
-func ReverseProxy() gin.HandlerFunc {
-    target := "http://localhost:8081" // hacer que sea random entre 8081 y 8082
-
-    url, err := url.Parse(target)
-    fmt.Print(err)
-    // checkErr(err)
-    proxy := httputil.NewSingleHostReverseProxy(url)
-    return func(c *gin.Context) {
-        proxy.ServeHTTP(c.Writer, c.Request)
-    }
-}
+import "math/rand"
+import "time"
 
 func main() {
 	router := gin.Default()
@@ -26,7 +16,33 @@ func main() {
 		})
 	})
 
-	router.GET("/test", ReverseProxy())
+	router.GET("/test", ReverseProxy(RandomServer())) // ver como hacer para que la url sea wildcard
 
 	router.Run(":8080") // listen and serve on 0.0.0.0:8080
+}
+
+func ReverseProxy(target string) gin.HandlerFunc {
+    fmt.Print("-------------- exec reverse proxy --------------------\n")
+
+    url, err := url.Parse(target)
+    fmt.Print(err)
+    // checkErr(err)
+    proxy := httputil.NewSingleHostReverseProxy(url)
+    return func(c *gin.Context) {
+        proxy.ServeHTTP(c.Writer, c.Request)
+    }
+}
+
+func RandomServer() string {
+    fmt.Print("-------------- exec random server --------------------\n") // se esta ejecutando una unica vez
+
+    rand.Seed(time.Now().Unix())
+
+    target_list := []string{
+        "http://localhost:8081",
+        "http://localhost:8082",
+    }
+    n := rand.Int() % len(target_list)
+
+    return target_list[n]
 }
